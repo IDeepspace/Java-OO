516fc8b1e5ddbaa1a26404fa4e968aa569bb9247
IDeepspace <cxin1427@qq.com>
2018-03-20 09:23:35 +0800

    completed practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,25 @@
 package practice01;
 
 public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String myName, int  myAge) {
+        name = myName;
+        age = myAge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+
+    }
 }
