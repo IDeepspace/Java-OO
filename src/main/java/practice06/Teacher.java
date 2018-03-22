77bb41eef490b8bd4da871cd9277e638578c85d7
IDeepspace <cxin1427@qq.com>
2018-03-22 11:42:49 +0800

    completed practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,35 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+
+    private String isTeaching(String klassNum) {
+        return super.introduce() + " I am a Teacher. I teach " + klassNum + ".";
+    }
+
+    @Override
+    public String introduce() {
+
+        if (this.klass == 0) {
+            return isTeaching("No Class");
+        }else {
+            return isTeaching("Class " + this.klass);
+        }
+    }
+
 }
